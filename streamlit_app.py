3fddbe9cc5dea5334cf6b0af193cca1bcad54725
Sai Prakash <63905195+SaiSpr@users.noreply.github.com>
2023-03-27 17:40:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -126,10 +126,7 @@ st.sidebar.image("1200px-Home_Credit_&_Finance_Bank.svg.png", use_column_width=T
 
 
 
-# Liste clients id sidebar 
-list_client_prod = df_test_prod['SK_ID_CURR'].tolist()
-client_id = st.sidebar.selectbox("Client Id list",list_client_prod)
-client_id = int(client_id)
+
 
 
 
@@ -141,65 +138,33 @@ st.sidebar.header('Input Features of The Transaction')
 sender_name = st.sidebar.text_input("""Input Sender ID""")
 receiver_name = st.sidebar.text_input("""Input Receiver ID""")
 step = st.sidebar.slider("""Number of Hours it took the Transaction to complete: """)
-types = st.sidebar.subheader(f"""
-                 Enter Type of Transfer Made:\n\n\n\n
-                 0 for 'Cash In' Transaction\n 
-                 1 for 'Cash Out' Transaction\n 
-                 2 for 'Debit' Transaction\n
-                 3 for 'Payment' Transaction\n  
-                 4 for 'Transfer' Transaction\n""")
-types = st.sidebar.selectbox("",(0,1,2,3,4))
-x = ''
-if types == 0:
-    x = 'Cash in'
-if types == 1:
-    x = 'Cash Out'
-if types == 2:
-    x = 'Debit'
-if types == 3:
-    x = 'Payment'
-if types == 4:
-    x =  'Transfer'
-    
-amount = st.sidebar.number_input("Amount in $",min_value=0, max_value=110000)
-oldbalanceorg = st.sidebar.number_input("""Sender Balance Before Transaction was made""",min_value=0, max_value=110000)
-newbalanceorg= st.sidebar.number_input("""Sender Balance After Transaction was made""",min_value=0, max_value=110000)
-oldbalancedest= st.sidebar.number_input("""Recipient Balance Before Transaction was made""",min_value=0, max_value=110000)
-newbalancedest= st.sidebar.number_input("""Recipient Balance After Transaction was made""",min_value=0, max_value=110000)
-isflaggedfraud = 0
-if amount >= 200000:
-  isflaggedfraud = 1
-else:
-  isflaggedfraud = 0
+
+ 
+# Liste clients id sidebar 
+list_client_prod = df_test_prod['SK_ID_CURR'].tolist()
+client_id = st.sidebar.selectbox("Client Id list",list_client_prod)
+# types = int(client_id)
+
+
+oldbalanceorg = int(client_id)
 
 
 if st.button("Detection Result"):
     values = {
-    "step": step,
-    "types": types,
-    "amount": amount,
-    "oldbalanceorig": oldbalanceorg,
-    "newbalanceorig": newbalanceorg,
-    "oldbalancedest": oldbalancedest,
-    "newbalancedest": newbalancedest,
-    "isflaggedfraud": isflaggedfraud
+
+    "oldbalanceorig": oldbalanceorg
     }
 
 
     st.write(f"""### These are the transaction details:\n
     Sender ID: {sender_name}
     Receiver ID: {receiver_name}
-    1. Number of Hours it took to complete: {step}\n
-    2. Type of Transaction: {x}\n
-    3. Amount Sent: {amount}$\n
+
     4. Sender Balance Before Transaction: {oldbalanceorg}$\n
-    5. Sender Balance After Transaction: {newbalanceorg}$\n
-    6. Recepient Balance Before Transaction: {oldbalancedest}$\n
-    7. Recepient Balance After Transaction: {newbalancedest}$\n
-    8. System Flag Fraud Status(Transaction amount greater than $200000): {isflaggedfraud}
+
                 """)
     
-    id = client_id
+    id = oldbalanceorg
     st.write(f"""Client_id is {id}""")
     res = requests.post(f"https://credit-card-production.up.railway.app/predict",json=values)
     json_str = json.dumps(res.json())
