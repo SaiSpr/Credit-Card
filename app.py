2feef95057df1b1937db5ab99113a3721a80b370
Sai Prakash <63905195+SaiSpr@users.noreply.github.com>
2023-03-28 12:53:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import uvicorn
 import joblib
 import numpy as np
 from pydantic import BaseModel
+import pandas as pd 
+import json
 
 
 
@@ -28,10 +30,59 @@ favicon_path = 'favicon.png'
 @app.get('/favicon.png', include_in_schema=False)
 async def favicon():
     return FileResponse(favicon_path)
-																	
+
+
+#import dataframe of test customer data
+
+df_test_prod = pd.read_csv('df_test_ok_prod_100_V7.csv', index_col=[0])
+# supprimer target
+df_test_prod.drop(columns=['TARGET'], inplace=True)
+# mettre SK_ID_CURR en index 
+df_test_prod_request  = df_test_prod.set_index('SK_ID_CURR')
+# Création list des clients 
+clients_id = df_test_prod["SK_ID_CURR"].tolist() 
+
+
+
 class fraudDetection(BaseModel):
 	
     oldbalanceorig:int	
+
+
+@app.post('/predict')
+def predict(data : fraudDetection):
+                                                                                                                                                                                                                                
+    id = data.oldbalanceorig
+    
+    if id not in clients_id:
+	raise HTTPException(status_code=404, detail="client's id not found")
+
+    else:
+
+	pipe_prod = joblib.load('LGBM_pipe_version7.pkl')
+
+	values_id_client = df_test_prod_request.loc[[id]]
+
+	# Définir le best threshold
+	prob_preds = pipe_prod.predict_proba(values_id_client)
+
+	#Fast_API_prob_preds
+	threshold = 0.332# definir threshold ici
+	y_test_prob = [1 if prob_preds[i][1]> threshold else 0 for i in range(len(prob_preds))]
+
+
+	return {
+		"prediction": y_test_prob[0],
+		"probability_0" : prob_preds[0][0],
+		"probability_1" : prob_preds[0][1],}
+	
+
+
+
+@app.post("/flow")
+def flow(name: str, age: int, email: str):
+    # do something with the data
+    return {"message": "Data received and processed"}
 
 
 
